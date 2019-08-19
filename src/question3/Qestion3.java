21e2c4a028d68c56780b1be26dfd41082f1f1956
amankushwaha577 <aman.kushwaha_cs18@gla.ac.in>
2019-08-19 15:23:14 +0530

    Add positive number identification

---
+++ src/question3/Qestion3.java
@@ -0,0 +1,17 @@
+package question3;
+
+import java.util.Scanner;
+
+public class Qestion3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        System.out.println(a + " % " + b + " = " + (a % b));
+
+    }
+}
